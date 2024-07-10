6287365f1cc24ae48a5195c97f365917878a5673
Sam Goldman <samgoldman12@gmail.com>
2024-07-10 16:27:43 -0400

    a

--- test/handlers/anthropic.test.ts
+++ test/handlers/anthropic.test.ts
@@ -1,34 +1,21 @@
-<<<<<<< HEAD
-import { describe, it, expect } from 'vitest';
-import { convertMessages, convertToolParams } from '../../src/handlers/anthropic';
-import { CompletionParams } from '../../src/chat';
-import { getDummyTool } from '../dummy';
-=======
 import { describe, expect, it } from 'vitest'
 
 import { CompletionParams } from '../../src/chat'
 import { convertMessages } from '../../src/handlers/anthropic'
->>>>>>> 9e80371c93b20655535ab6d6381f6d7e7632c944
 
 describe('convertMessages', () => {
-  it('converts user and system messages into a block with system messages prefixed', () => {
+  it('converts user and system messages into a block with system messages prefixed', async () => {
     const input: CompletionParams['messages'] = [
       { role: 'user', content: 'Hi' },
       { role: 'system', content: 'Bye' },
     ]
     const output = [
-      {
-        role: 'user',
-        content: [
-          { type: 'text', text: 'Hi' },
-          { type: 'text', text: 'System: Bye' },
-        ],
-      },
-    ]
-    expect(convertMessages(input)).toEqual(output)
-  })
+      { role: 'user', content: [{ type: 'text', text: 'Hi' }, { type: 'text', text: 'System: Bye' }] }
+    ];
+    expect(await convertMessages(input)).toEqual(output);
+  });
 
-  it('converts alternating user and assistant messages into blocks', () => {
+  it('converts alternating user and assistant messages into blocks', async () => {
     const input: CompletionParams['messages'] = [
       { role: 'user', content: 'Hi' },
       { role: 'assistant', content: 'Yo' },
@@ -37,12 +24,12 @@ describe('convertMessages', () => {
     const output = [
       { role: 'user', content: [{ type: 'text', text: 'Hi' }] },
       { role: 'assistant', content: [{ type: 'text', text: 'Yo' }] },
-      { role: 'user', content: [{ type: 'text', text: 'Bye' }] },
-    ]
-    expect(convertMessages(input)).toEqual(output)
-  })
+      { role: 'user', content: [{ type: 'text', text: 'Bye' }] }
+    ];
+    expect(await convertMessages(input)).toEqual(output);
+  });
 
-  it('prepends an empty user message when the input messages start with an assistant message', () => {
+  it('prepends an empty user message when the input messages start with an assistant message', async () => {
     const input: CompletionParams['messages'] = [
       { role: 'assistant', content: 'Yo' },
       { role: 'user', content: 'Bye' },
@@ -51,28 +38,22 @@ describe('convertMessages', () => {
       { role: 'user', content: [{ type: 'text', text: 'Empty' }] },
       { role: 'assistant', content: [{ type: 'text', text: 'Yo' }] },
       { role: 'user', content: [{ type: 'text', text: 'Bye' }] },
-    ]
-    expect(convertMessages(input)).toEqual(output)
-  })
+    ];
+    expect(await convertMessages(input)).toEqual(output);
+  });
 
-  it('does not prepend a user message when the input messages start with a system message', () => {
+  it('does not prepend a user message when the input messages start with a system message', async () => {
     const input: CompletionParams['messages'] = [
       { role: 'system', content: 'Hello' },
       { role: 'user', content: 'Hi' },
     ]
     const output = [
-      {
-        role: 'user',
-        content: [
-          { type: 'text', text: 'System: Hello' },
-          { type: 'text', text: 'Hi' },
-        ],
-      },
-    ]
-    expect(convertMessages(input)).toEqual(output)
-  })
+      { role: 'user', content: [{ type: 'text', text: 'System: Hello' }, { type: 'text', text: 'Hi' }] }
+    ];
+    expect(await convertMessages(input)).toEqual(output);
+  });
 
-  it('groups consecutive system messages into a single user message', () => {
+  it('groups consecutive system messages into a single user message', async () => {
     const input: CompletionParams['messages'] = [
       { role: 'system', content: 'Hello' },
       { role: 'system', content: 'World' },
@@ -81,12 +62,12 @@ describe('convertMessages', () => {
     const output = [
       { role: 'user', content: [{ type: 'text', text: 'System: Hello' }, { type: 'text', text: 'System: World' }, { type: 'text', text: 'Hi' }] }
     ];
-    expect(convertMessages(input)).toEqual(output);
+    expect(await convertMessages(input)).toEqual(output);
   });
 });
 
 describe('convertToolParams', () => {
   it(`returns undefined when 'tool_choice' is 'none'`, () => {
-    expect(convertToolParams('none', [getDummyTool()])).equals(undefined)
+    expect(convertToolParams('none', [getDummyTool()])).toEqual({toolChoice: undefined, tools: undefined})
   })
 })
